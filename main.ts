e340f32be0b3a6896df33e3b8f9f0985f07b5aac
dannybalanta <42857316+dannybalanta@users.noreply.github.com>
2020-11-02 14:04:38 -0500

    Changing while loop for conditional if and getting same result

--- main.ts
+++ main.ts
@@ -1,22 +1,20 @@
 basic.forever(function () {
     basic.showNumber(input.temperature())
     basic.showString("°C")
-    while (input.temperature() >= 26 && input.temperature() <= 34) {
+    // if (input.temperature() < 26)
+    if (input.temperature() >= 26 && input.temperature() <= 34) {
         basic.showIcon(IconNames.Happy)
         basic.pause(500)
         basic.clearScreen()
         basic.showString("T ok")
         basic.pause(500)
-    }
-    // if (input.temperature() < 26)
-    while (input.temperature() > 34) {
+    } else if (input.temperature() > 34) {
         basic.showIcon(IconNames.No)
         basic.pause(500)
         basic.clearScreen()
         basic.showString("T alta")
         basic.pause(500)
-    }
-    while (input.temperature() < 26) {
+    } else {
         basic.showIcon(IconNames.Sad)
         basic.pause(500)
         basic.clearScreen()
